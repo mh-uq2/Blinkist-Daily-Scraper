1e67910ccdcb028073d5cb49f18dd1df14b5c8d2
Vivek Jain <theghostofc@gmail.com>
2018-04-27 22:15:04 +0530

    Remove unused code

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 from bs4 import BeautifulSoup
 from datetime import datetime
@@ -14,11 +13,6 @@ def get_element_from_request(url, element, class_):
     return soup.find(element, class_ = class_)
 
 # Get meta data
-# daily = 'https://www.blinkist.com/nc/daily'
-# response = http.request('GET', daily)
-
-# soup = BeautifulSoup(response.data.decode('utf-8'), "html5lib")
-# container = soup.find('div', class_="dailyV2__free-book__container")
 container = get_element_from_request('https://www.blinkist.com/nc/daily', 'div', "dailyV2__free-book__container")
 
 title = container.find('div', 'dailyV2__free-book__title').string.strip()
@@ -26,13 +20,8 @@ author = container.find('div', 'dailyV2__free-book__author').string.strip()
 cta = container.find('div', 'dailyV2__free-book__cta').a['href']
 
 # Get actual content
-# url = f'https://www.blinkist.com{cta}'
-# response = http.request('GET', url)
-
-# soup = BeautifulSoup(response.data.decode('utf-8'), "html5lib")
-# article = soup.find('article')
-
 article = get_element_from_request(f'https://www.blinkist.com{cta}', 'article', 'shared__reader__blink reader__container__content')
+
 # Convert to markdown and dump to a file
 output = tomd.convert(str(article).strip())
 date = datetime.now().strftime('%Y%m%d')
